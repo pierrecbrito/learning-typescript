f91e4112a9ed55a3197e5bd7226d8cd41e87be0e
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-11 19:54:05 -0300

    arrays

--- tipos/index.js
+++ tipos/index.js
@@ -2,3 +2,7 @@
 // Array
 const numbers = [1, 2, 3, 4, 5];
 console.log(numbers);
+const numbers2 = [1, 2, 3, 4, 5];
+console.log(numbers2);
+const words = ["Hello", "World"];
+console.log(words);
--- tipos/index.ts
+++ tipos/index.ts
@@ -1,3 +1,9 @@
 // Array
 const numbers: number[] = [1, 2, 3, 4, 5];
 console.log(numbers);
+
+const numbers2: Array<number> = [1, 2, 3, 4, 5];
+console.log(numbers2);
+
+const words: string[] = ["Hello", "World"];
+console.log(words);
