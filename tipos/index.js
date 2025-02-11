592b1899d7e5678ca1818709474b756a1927e85b
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-11 20:52:08 -0300

    Anonymous functions

--- tipos/index.js
+++ tipos/index.js
@@ -6,3 +6,13 @@ const numbers2 = [1, 2, 3, 4, 5];
 console.log(numbers2);
 const words = ["Hello", "World"];
 console.log(words);
+// Whatever type satisfies the condition
+const whatever = [1, "Hello", true];
+console.log(whatever);
+const doIt = (a, b) => a + b;
+console.log(doIt(1, 2));
+//In Anonymous functions
+setTimeout(() => {
+    const sallary = 1000;
+    console.log(sallary);
+}, 2000);
--- tipos/index.ts
+++ tipos/index.ts
@@ -7,3 +7,19 @@ console.log(numbers2);
 
 const words: string[] = ["Hello", "World"];
 console.log(words);
+
+// Whatever type satisfies the condition
+const whatever: any[] = [1, "Hello", true];
+console.log(whatever);
+
+const doIt = (a: number, b: number):number =>  a + b ;
+console.log(doIt(1, 2));
+
+//In Anonymous functions
+setTimeout(() => {
+    const sallary: number = 1000;
+
+    console.log(sallary);
+}, 2000);
+
+//Objects
