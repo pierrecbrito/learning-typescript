af12013748537d14a1ef7543b1a5d050f166c860
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-12 17:43:17 -0300

    Narrowing challenge

--- narrowing/index.js
+++ narrowing/index.js
@@ -38,3 +38,29 @@ console.log(person1 instanceof Person, person1);
 //"IN"
 let obj = { name: 'John', age: 30 };
 console.log('name' in obj, 'age' in obj, 'birthday' in obj);
+function reveiceReview(review) {
+    if (!review) {
+        console.log('Review: Not received');
+        return;
+    }
+    if (typeof review === 'number') {
+        if (review == 1) {
+            console.log('Uma pena você não ter curtido.');
+        }
+        else if (review == 2) {
+            console.log('Onde erramos?');
+        }
+        else if (review == 3) {
+            console.log('Bom. Onde você acha que podemos melhorar?');
+        }
+        else if (review == 4) {
+            console.log('Que bom que gostou!');
+        }
+        else if (review == 5) {
+            console.log('Obrigado!');
+        }
+    }
+}
+reveiceReview(1);
+reveiceReview(2);
+reveiceReview(false);
--- narrowing/index.ts
+++ narrowing/index.ts
@@ -47,3 +47,30 @@ console.log(person1 instanceof Person, person1);
 
 let obj = { name: 'John', age: 30 };
 console.log('name' in obj, 'age' in obj, 'birthday' in obj);
+
+type Review = 1 | 2 | 3 | 4 | 5 | boolean;
+
+function reveiceReview(review: Review) {
+    if(!review) {
+        console.log('Review: Not received');
+        return;
+    }
+
+    if(typeof review === 'number') {
+        if(review == 1) {
+            console.log('Uma pena você não ter curtido.');
+        } else if(review == 2) {
+            console.log('Onde erramos?');
+        } else if(review == 3) {
+            console.log('Bom. Onde você acha que podemos melhorar?');
+        } else if(review == 4) {
+            console.log('Que bom que gostou!');
+        } else if(review == 5) {
+            console.log('Obrigado!');
+        }
+    }
+} 
+
+reveiceReview(1);
+reveiceReview(2);
+reveiceReview(false);
