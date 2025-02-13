9a1d273d589b44bb5d048640a51f59221c9e3658
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-13 18:37:59 -0300

    function as parameter

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,11 @@ function withoutReturn() {
     console.log('I am a void function');
 }
 withoutReturn();
+//2- number
+function greeting(name) {
+    return `Hello ${name}`;
+}
+function preGreeting(f, username) {
+    console.log("Preparing function...", f(username));
+}
+console.log(preGreeting(greeting, 'John'));
--- functions/index.ts
+++ functions/index.ts
@@ -4,3 +4,14 @@ function withoutReturn(): void {
 }
 
 withoutReturn();
+
+//2- function as parameter
+function greeting(name: string): string {
+    return `Hello ${name}`;
+}
+
+function preGreeting(f: (name:string) => string, username: string) {
+    console.log("Preparing function...", f(username));
+}
+
+console.log(preGreeting(greeting, 'John'));
