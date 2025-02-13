dabb5b79638d36f494cfa39204d8c6f5ac21ed9f
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-13 19:46:35 -0300

    Interfaces: Optional properties

--- objects/index.js
+++ objects/index.js
@@ -4,3 +4,13 @@ function showProductDetails(product) {
 }
 const shirt = { name: 'Shirt', price: 20, isAvaliable: true };
 showProductDetails(shirt);
+function showProductDetails2(product) {
+    if (!product.isAvaliable) {
+        console.log(`Product: ${product.name}, Price: ${product.price}`);
+    }
+    else {
+        console.log(`Product: ${product.name}, Price: ${product.price}, Avaliable: false`);
+    }
+}
+const shirt2 = { name: 'Shirt', price: 20 };
+showProductDetails2(shirt2);
--- objects/index.ts
+++ objects/index.ts
@@ -11,3 +11,21 @@ function showProductDetails(product: Product) {
 
 const shirt: Product = {name: 'Shirt', price: 20, isAvaliable: true};
 showProductDetails(shirt);
+
+//Optional properties
+interface Product2 {
+    name: string;
+    price: number;
+    isAvaliable?: boolean;
+}
+
+function showProductDetails2(product: Product2) {
+    if (!product.isAvaliable) {
+        console.log(`Product: ${product.name}, Price: ${product.price}`);
+    } else {
+        console.log(`Product: ${product.name}, Price: ${product.price}, Avaliable: false`);
+    }
+}
+
+const shirt2: Product2 = {name: 'Shirt', price: 20};
+showProductDetails2(shirt2);
