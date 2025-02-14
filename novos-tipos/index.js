9540ff6acba6b49dabcba0accd72c46a9c9469bd
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-13 21:15:38 -0300

    Constraint in Generics

--- novos-tipos/index.js
+++ novos-tipos/index.js
@@ -5,3 +5,9 @@ function showDate(arg) {
 }
 console.log(showDate('12/12/2020'));
 console.log(showDate(12));
+//Constaints em generics
+function showProductName(obj) {
+    return obj.name;
+}
+const myObj = { name: 'John', age: 30 };
+console.log(showProductName(myObj));
--- novos-tipos/index.ts
+++ novos-tipos/index.ts
@@ -5,3 +5,12 @@ function showDate<T>(arg: T): string {
 
 console.log(showDate('12/12/2020'));
 console.log(showDate(12));
+
+//Constaints em generics
+
+function showProductName<T extends {name: string}>(obj: T): string {
+    return obj.name;
+}
+
+const myObj = {name: 'John', age: 30};
+console.log(showProductName(myObj));
