76f0628b852d0c181f6cdd7f6332cf39b7aa3854
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-15 19:38:07 -0300

    alias

--- modulos/index.js
+++ modulos/index.js
@@ -7,3 +7,9 @@ const greet_js_1 = __importDefault(require("./greet.js"));
 (0, greet_js_1.default)();
 const variable_js_1 = require("./variable.js");
 console.log(variable_js_1.x); // 10
+const multiple_js_1 = require("./multiple.js");
+console.log(multiple_js_1.a); // 10
+console.log(multiple_js_1.b); // teste
+(0, multiple_js_1.myFunction)(); // Olá
+const changename_js_1 = require("./changename.js");
+console.log(changename_js_1.a); // 10
--- modulos/index.ts
+++ modulos/index.ts
@@ -4,3 +4,11 @@ importGreet()
 
 import { x } from './variable.js'
 console.log(x) // 10
+
+import { a, b, myFunction } from './multiple.js'   
+console.log(a) // 10
+console.log(b) // teste
+myFunction() // Olá
+
+import { a as a1} from './changename.js'
+console.log(a1) // 10
