9abde3de587a11544654472f01a840b724872efa
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-14 22:48:39 -0300

    Radonyl fields and Inheritance

--- classes/index.js
+++ classes/index.js
@@ -6,3 +6,41 @@ const matheus = new User();
 //matheus.name = 'Matheus';   
 //matheus.age = 30;
 console.log(matheus);
+//Constuctor
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+        this.name = name;
+        this.year = year;
+    }
+}
+const myCar = new Car('Fusca', 1970);
+console.log(myCar);
+//Readonly fields
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const dog = new Animal('Dog');
+//dog.name = 'Cat';
+console.log(dog);
+//Inheritance
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.name = name;
+        this.age = age;
+    }
+}
+class Employee extends Person {
+    constructor(name, age, salary) {
+        super(name, age);
+        this.salary = salary;
+        this.salary = salary;
+    }
+}
+const employee = new Employee('John', 30, 2000);
+console.log(employee);
--- classes/index.ts
+++ classes/index.ts
@@ -8,3 +8,44 @@ const matheus = new User();
 //matheus.name = 'Matheus';   
 //matheus.age = 30;
 console.log(matheus);
+
+//Constuctor
+class Car {
+    constructor(public name: string, public year: number) {
+        this.name = name;
+        this.year = year;
+    }
+}
+
+const myCar = new Car('Fusca', 1970);
+console.log(myCar);
+
+//Readonly fields
+class Animal {
+    readonly name: string
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const dog = new Animal('Dog');
+//dog.name = 'Cat';
+console.log(dog);
+
+//Inheritance
+class Person {
+    constructor(public name: string, public age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Employee extends Person {
+    constructor(name: string, age: number, public salary: number) {
+        super(name, age);
+        this.salary = salary;
+    }
+}
+
+const employee = new Employee('John', 30, 2000);
+console.log(employee);
