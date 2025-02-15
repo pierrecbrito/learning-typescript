dc88d0727565b857bf27d8a09f625c145903a94d
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-14 22:43:55 -0300

    typescript with class

---
+++ classes/index.js
@@ -0,0 +1,8 @@
+"use strict";
+//Classes
+class User {
+}
+const matheus = new User();
+//matheus.name = 'Matheus';   
+//matheus.age = 30;
+console.log(matheus);
---
+++ classes/index.ts
@@ -0,0 +1,10 @@
+//Classes
+class User {
+    name!: string
+    age!: number
+}
+
+const matheus = new User();
+//matheus.name = 'Matheus';   
+//matheus.age = 30;
+console.log(matheus);
